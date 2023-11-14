f70527590426912ad02d2b38c7a110a6f8d49b74
Mateus Martins Freitas <mateusmfreitas12@gmail.com>
2023-11-14 16:00:06 -0300

    Adicionado gráfico de chamados por setor

--- src/telas/admin/graficos.js
+++ src/telas/admin/graficos.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
 import GraficoChamadosBarra from '../../componentes/graficoChamadosBarra';
 import GraficoChamadosResolvidos from '../../componentes/graficoChamadosResolvidos';
+import GraficoChamadosSetor from '../../componentes/graficoChamadosSetor';
+
 
 export default function Graficos({ navigation }) {
  return( 
-    <View>
+    <ScrollView>
         <GraficoChamadosBarra></GraficoChamadosBarra>
         <GraficoChamadosResolvidos></GraficoChamadosResolvidos>
-    </View>
+        <GraficoChamadosSetor></GraficoChamadosSetor>
+    </ScrollView>
  );
 
 }
